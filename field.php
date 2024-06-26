ef4f82d0414faaf666ed9866380faeb8184ff7b2
Amadeu Arderiu <amadeu@sirvelia.com>
2024-06-26 17:02:28 +0200

    fix for bedrock

--- field.php
+++ field.php
@@ -3,12 +3,18 @@
 use Carbon_Fields\Carbon_Fields;
 use Carbon_Field_Free_Select\Free_Select_Field;
 
+if (!function_exists('add_action')) {
+	return;
+}
+
 define('Carbon_Field_Free_Select\\DIR', __DIR__ );
 
-Carbon_Fields::extend( Free_Select_Field::class, function( $container ) {
-	return new Free_Select_Field(
-		$container['arguments']['type'],
-		$container['arguments']['name'],
-		$container['arguments']['label']
-	);
-} );
+add_action('after_setup_theme', function () {
+	Carbon_Fields::extend( Free_Select_Field::class, function( $container ) {
+		return new Free_Select_Field(
+			$container['arguments']['type'],
+			$container['arguments']['name'],
+			$container['arguments']['label']
+		);
+	} );
+}, 99);

ea8cfb341ddcd27576a253904be43534e147d062
Amadeu <46872149+amadeuarderiu@users.noreply.github.com>
2024-06-26 17:13:05 +0200

    Update field.php

--- field.php
+++ field.php
@@ -3,11 +3,11 @@
 use Carbon_Fields\Carbon_Fields;
 use Carbon_Field_Free_Select\Free_Select_Field;
 
+define('Carbon_Field_Free_Select\\DIR', __DIR__ );
+
 if (!function_exists('add_action')) {
 	return;
 }
-
-define('Carbon_Field_Free_Select\\DIR', __DIR__ );
 
 add_action('after_setup_theme', function () {
 	Carbon_Fields::extend( Free_Select_Field::class, function( $container ) {
